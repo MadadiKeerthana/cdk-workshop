24e97646e4c749b7078d3756cf276640d47a8649
Keerthana <48146190+MadadiKeerthana@users.noreply.github.com>
2024-07-23 14:36:20 -0700

    testing

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -5,6 +5,8 @@ import { Construct } from 'constructs';
 
 export interface HitCounterProps {
     downstream: lambda.IFunction;
+
+    readCapacity?: number;
 }
 
 export class HitCounter extends Construct {
@@ -14,10 +16,15 @@ export class HitCounter extends Construct {
     public readonly table: dynamoDB.Table;
 
     constructor(scope: Construct, id: string, props: HitCounterProps) {
+        if (props.readCapacity !== undefined && (props.readCapacity < 5 || props.readCapacity > 20)) {
+            throw new Error('readCapacity must be greater than 5 and less than 20');
+        }
         super(scope, id);
 
         const table = new dynamoDB.Table(this, 'Hits', {
             partitionKey: { name: 'path', type: dynamoDB.AttributeType.STRING },
+            encryption: dynamoDB.TableEncryption.AWS_MANAGED,
+            readCapacity: props.readCapacity ?? 5,
             removalPolicy: cdk.RemovalPolicy.DESTROY
         });
         this.table = table
